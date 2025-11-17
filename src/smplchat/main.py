ece058614d35eb92302b2a452cf66a6d16e4fc7d
Viljami Ilola <viljami.ilola@helsinki.fi>
2025-11-17 14:20:57 +0200

    Adds comments for some sort of skeleton

--- src/smplchat/main.py
+++ src/smplchat/main.py
@@ -3,6 +3,21 @@
 def main():
     """ main - the entry point to the application """
     print("It's alive")
+    
+    # initialize iplist & history
+    # initialize listener
+    # initialize dispatcher
+    # initialize message-mangler(iplist,history,listener(packet-queue),dispatcher)
+    # initialize ui(history)
+    # main loop:
+    #	mangle loop:
+    #		message-mangler-process-queue
+    #	UI: do action from ui for example:
+    #		join
+    #		quit
+    #		send message -> message-mangler
+
+    
 
 if __name__ == "__main__":
     main()
