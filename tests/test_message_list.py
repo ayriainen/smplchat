18049a3fe6a49b7e82172453ce73145f955c5846
Viljami Ilola <viljami.ilola@helsinki.fi>
2025-11-22 15:38:13 +0200

    tests: is messagelist history added correctly

--- tests/test_message_list.py
+++ tests/test_message_list.py
@@ -1,15 +1,17 @@
 import unittest
 
 from smplchat.message_list import MessageList, MessageEntry
-from smplchat.packet_mangler import ChatRelayMessage
+from smplchat.packet_mangler import (
+    ChatRelayMessage,
+    JoinRelayMessage,
+    LeaveRelayMessage )
 
 class TestMessageList(unittest.TestCase):
     def test_init(self):
-        ml = MessageList()
-
-    def test_add(self):
-        ml = MessageList()
-        ml.add(ChatRelayMessage(
+        self.ml = MessageList()
+    
+    def add_chat1(self):
+        self.ml.add(ChatRelayMessage(
             msg_type = 0,
             uniq_msg_id = 3,
             sender_ip = 55,
@@ -17,14 +19,9 @@ class TestMessageList(unittest.TestCase):
             old_message_ids = [],
             sender_nick = "tester",
             msg_text = "testing yeah" ))
-        self.assertEqual(ml.get(), [
-            MessageEntry( uid=3, seen=1, time=666,
-            nick='tester', message='testing yeah') ])
-        return ml
-            
-    def test_addmore(self):
-        ml = self.test_add()
-        ml.add(ChatRelayMessage(
+
+    def add_chat2(self):
+        self.ml.add(ChatRelayMessage(
             msg_type = 0,
             uniq_msg_id = 5,
             sender_ip = 73,
@@ -32,10 +29,77 @@ class TestMessageList(unittest.TestCase):
             old_message_ids = [],
             sender_nick = "tester2",
             msg_text = "testing hell yeah" ))
-        self.assertEqual(ml.get(), [
+
+    def add_chat_with_history(self):
+        self.ml.add(ChatRelayMessage(
+            msg_type = 0,
+            uniq_msg_id = 55,
+            sender_ip = 73,
+            sender_local_time = 777,
+            old_message_ids = [3,5],
+            sender_nick = "hisory-person",
+            msg_text = "history testing" ))
+
+    def add_join(self):
+        self.ml.add(JoinRelayMessage(
+            msg_type = 1,
+            uniq_msg_id = 4,
+            sender_ip = 62,
+            sender_local_time = 7334,
+            old_message_ids = [],
+            sender_nick = "joiner" ))
+
+    def add_leave(self):
+        self.ml.add(LeaveRelayMessage(
+            msg_type = 2,
+            uniq_msg_id = 9,
+            sender_ip = 92,
+            sender_local_time = 2346,
+            old_message_ids = [],
+            sender_nick = "leaver" ))
+
+
+    def test_add(self):
+        self.ml = MessageList()
+        self.add_chat1()
+        self.assertEqual(self.ml.get(), [
+            MessageEntry( uid=3, seen=1, time=666,
+            nick='tester', message='testing yeah') ])
+
+    def test_addmore(self):
+        self.ml = MessageList()
+        self.add_chat1()
+        self.add_chat2()
+        self.assertEqual(self.ml.get(), [
             MessageEntry( uid=3, seen=1, time=666,
             nick='tester', message='testing yeah'),
             MessageEntry( uid=5, seen=1, time=777,
             nick='tester2', message='testing hell yeah'),
              ])
-        return ml
+
+    def test_joinleave(self):
+        self.ml = MessageList()
+        self.add_chat1()
+        self.add_chat2()
+        self.add_join()
+        self.add_leave()
+        self.assertEqual(self.ml.get(), [
+            MessageEntry( uid=3, seen=1, time=666,
+            nick='tester', message='testing yeah'),
+            MessageEntry( uid=5, seen=1, time=777,
+            nick='tester2', message='testing hell yeah'),
+            MessageEntry(uid=4, seen=1, time=7334,
+            nick='joiner', message='*** joined the chat'),
+            MessageEntry(uid=9, seen=1, time=2346,
+            nick='leaver', message='*** left the chat') ])
+
+    def test_history_add(self):
+        self.ml = MessageList()
+        self.add_chat_with_history()
+        self.assertEqual(self.ml.get(), [
+            MessageEntry( uid=3, seen=0, time=0,
+            nick='system', message='<3> message pending'),
+            MessageEntry( uid=5, seen=0, time=0,
+            nick='system', message='<5> message pending'),
+            MessageEntry(uid=55, seen=1, time=777,
+            nick='hisory-person', message='history testing')])
