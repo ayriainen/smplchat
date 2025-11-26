3314298ca60f40c411f770772b326a94fb7fd550
Viljami Ilola <viljami.ilola@helsinki.fi>
2025-11-26 12:26:31 +0200

    Adds old message request and reply type messages to message_gen

--- src/smplchat/message/message_gen.py
+++ src/smplchat/message/message_gen.py
@@ -6,7 +6,9 @@ from .message import (
     JoinRelayMessage,
     LeaveRelayMessage,
     JoinRequestMessage,
-    JoinReplyMessage)
+    JoinReplyMessage,
+    OldRequestMessage,
+    OldReplyMessage)
 
 def new_message(msg_type: MessageType, **kwargs):
     """ Generates new message. TODO: move this in better place
@@ -14,12 +16,14 @@ def new_message(msg_type: MessageType, **kwargs):
         
         Optional parameters:
         nick		- Sets nick of sender
-        text		- Actual text of message (used in CHAT_RELAY)
+        text		- Actual text of message (used in CHAT_RELAY, OLD_REPLY)
         ip		- Neened in RELAY messages to distribute ipadresses
                           and update keepalive status.
         msg_list	- Needed in RELAY messages and JOIN_REPLY for
                           for gathiring information of past messages
-        client_list	- Needed only in JOIN_REPLY 
+        client_list	- Needed only in JOIN_REPLY
+        uid		- uid of message. Needen in OLD_REQUEST and OLD_REPLY
+        old_type		- Type on old message. Needed in OLD_REPLY        
     """
     uid = generate_uid()
     try:
@@ -60,6 +64,19 @@ def new_message(msg_type: MessageType, **kwargs):
                     msg_type = MessageType.JOIN_REPLY,
                     old_message_ids = kwargs["msg_list"].latest_ids(),
                     ip_addresses = kwargs["client_list"].get() )
+
+            case MessageType.OLD_REQUEST:
+                return OldRequestMessage(
+                    msg_type = MessageType.OLD_REQUEST,
+                    uniq_msg_id = kwargs["uid"] )
+
+            case MessageType.OLD_REPLY:
+                return OldReplyMessage(
+                    msg_type = MessageType.OLD_REPLY,
+                    old_msg_type = kwargs["old_type"],
+                    uniq_msg_id = kwargs["uid"],
+                    sender_nick = kwargs["nick"],
+                    msg_text = kwargs["text"] )
 
     except KeyError as e:
         dprint(f"Cannot create message. Problem with new_message parameters.\n{e}")
